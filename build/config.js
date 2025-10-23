cee88c33f6014c59d145f4e6262e9fbdcecc172c
AlexandreRoy-dev <alex@codesk.ca>
2025-10-23 19:31:57 -0400

    init

---
+++ build/config.js
@@ -0,0 +1,26 @@
+const config = {
+  path: {
+    html: ['./', './docs', './components'],
+    scss: 'src/scss',
+    src_js: 'src/js',
+    js: 'assets/js',
+    css: 'assets/css',
+    vendor: 'assets/vendor',
+  },
+  fileNames: {
+    css: 'theme',
+    js: 'theme',
+  },
+  jsBanner: `
+  /*!
+   * Silicon | Multipurpose Bootstrap Template & UI Kit
+   * Copyright 2025 Coderthemes
+   * Theme scripts
+   *
+   * @copyright Coderthemes
+   * @version 1.0
+   */
+  `,
+}
+
+export default config
